ceacb197f2fb312455a92aeba68bedec6d6b0346
Mario Marchiorell <mario.marchiorello@gmail.com>
2021-02-10 15:11:31 +0100

    mario first commit---clease excuse the puns, i couldn't resist

---
+++ mario.js
@@ -0,0 +1,90 @@
+// console.log("------------------ claculator Funtion------------------")
+
+// function calculator ( num1,num2,operator){
+    
+//     if ( num2 === 0 && operator === "/"){
+//         console.log(" I PETY THE FOOL WHO DEVIDES BY ZERO---- Mr.T");
+//     }else if( operator === "+"){
+//         console.log(num1 + num2);
+//     }else if( operator === "-"){
+//         console.log(num1 - num2);
+//     }else if( operator === "*"){
+//         console.log(num1 * num2);
+//     }else if( operator === "/"){
+//     console.log(num1 / num2);
+//     };
+// };
+
+// calculator(3,1,"+");
+
+
+
+// console.log("------------------ myEach Funtion------------------")
+// WORKS
+
+// let arr= [8,5,6,3,4];
+// const cb = parameter => parameter + parameter;
+// function myForEach(arr, cb) {
+//     var mapped = [];
+//      for (let i = 0; i < arr.length; i++){
+//          mapped.push(cb(arr[i]))
+//      }
+//     return mapped;
+//   }
+
+//   let test = myForEach(arr,cb)
+//   console.log(test)
+
+// console.log("------------------ merge Funtion------------------")
+// doesn't merge in the right order
+
+// const obj1 = { a: 3, b: 2 }
+// const obj2 ={ a: 2, c: 4 }
+// function mergeObjects (para1,para2,para3){
+//     let newObj = {}
+//     newObj = Object.assign(para1,para2,para3);
+//     console.log(newObj);
+// };
+
+// mergeObjects({ a: 3, b: 2 }, { a: 2, c: 4 }, { e: 8, c: 5});
+
+
+// console.log("------------------ word count Funtion------------------")
+
+
+// function wordCount (string){
+//     let StringLenght = string.split(" ").length;
+//     let cutToMaxLength = string.split(" ").slice(0,30).join(" ");
+//     StringLenght >= 29 ? console.log(`ERROR, it's too long (thats what she said), you wrote ${StringLenght} words. We can only allow the following ${cutToMaxLength}-.`)
+//     : console.log(StringLenght)
+// }
+// wordCount("Life is a series of natural and spontaneous changes. Don't resist them; that only creates sorrow. Let reality be reality. Let things flow naturally forward in whatever way they like. ― Lao Tzu")
+
+// console.log("------------------ print the century Funtion------------------")
+
+
+
+// const transformed = year.toString().split("");
+
+
+
+
+// const year=2100;
+// function printTheCentury (Number){    
+    
+    
+    
+
+// printTheCentury(year)
+
+/* 
+if the number length is <= 3 take  [0] index, add 1 and the matching string
+if the number length is >=4 take the [0 and 1] index, add one and the matching string
+
+
+*/ 
+
+
+
+
+
